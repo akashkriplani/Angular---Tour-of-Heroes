d296915d0ea49c83728eb7a894f8820112d656cb
akashkriplani <aakash23kriplani@gmail.com>
2019-09-04 12:42:15 +0530

    Created a message component and message service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('Hero Service: Fetched heroes');
     return of(HEROES);
   }
 
